987702b9114be8487acd8c2ffec9a51911b74f7d
chaubatkunda <maniklusi31@gmail.com>
2022-02-09 02:24:20 +0700

    page login dan register selesai

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -4,13 +4,19 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
+
 class GuestLayout extends Component
 {
+    public $title;
     /**
      * Get the view / contents that represents the component.
      *
      * @return \Illuminate\View\View
      */
+    public function __construct($title = null)
+    {
+        $this->title = $title ?? config('app.name');
+    }
     public function render()
     {
         return view('layouts.guest');
