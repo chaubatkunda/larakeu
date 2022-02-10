90c7f86a9ce2ccd932e9e31d181343a5f1fd27f4
chaubatkunda <maniklusi31@gmail.com>
2022-02-10 20:06:40 +0700

    finish

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(100)->create();
-        $this->call([
-            IncomeSeeder::class,
-            // ExpenditureSeeder::class
-        ]);
+        User::factory(100)->create();
+        // $this->call([
+        //     IncomeSeeder::class,
+        //     ExpenditureSeeder::class
+        // ]);
     }
 }
